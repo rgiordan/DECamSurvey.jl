629eb30d82d302b7cc31be7eb3e3bfbe930d5ba9
Ryan Giordano <rgiordan@gmail.com>
2017-03-15 23:25:43 -0700

    Legacy survey comparison

--- src/load_fits.jl
+++ src/load_fits.jl
@@ -15,7 +15,7 @@ f_mask = FITSIO.FITS(joinpath(data_path, join([ fname_head, "d", fname_tail ])))
 header = FITSIO.read_header(f_image[1]);
 asec_per_pixel = (header["PIXSCAL1"], header["PIXSCAL2"])
 
-im_ind = 3;
+im_ind = 4;
 
 image = FITSIO.read(f_image[im_ind]);
 weight = FITSIO.read(f_weight[im_ind]);
@@ -26,9 +26,8 @@ image[mask .== 1] = NaN
 im_header = FITSIO.read_header(f_image[im_ind]);
 im_header_string = FITSIO.read_header(f_image[im_ind], String);
 
-# Get the corners
+# Get the corners, confirm that WCS matches up with them.
 corners = [ [ im_header["COR$(i)RA1"], im_header["COR$(i)DEC1"]] for i in 1:4 ]
-
 wcs = WCS.from_header(im_header_string)[1]
 WCS.pix_to_world(wcs, Float64[1,1])
 WCS.pix_to_world(wcs, Float64[x for x in size(image)])
@@ -37,14 +36,19 @@ corners
 # arcmin
 [x for x in asec_per_pixel] .* [x for x in size(image)] / 60
 
-if false
-    trim_level = quantile(image[ !isnan(image[:]) ][:], 0.9999)
-    noise_level = median(image[ !isnan(image[:]) ][:]) + 1 * std(image[!isnan(image)])
+function get_trimmed_image(image; trim_quantile=0.9999, num_sd=2)
+    trim_level = quantile(image[ !isnan(image[:]) ][:], trim_quantile)
+    noise_level = median(image[ !isnan(image[:]) ][:]) + num_sd * std(image[!isnan(image)])
     image_trim = deepcopy(image);
     image_trim[image_trim .> trim_level] = trim_level;
     image_trim[image_trim .< noise_level] = 0
-    matshow(image_trim); colorbar()
-    # matshow(mask); colorbar()
-    # matshow(weight); colorbar()
+    return image_trim
 end
+
+# Note that relative to the legacy viewer
+# http://legacysurvey.org
+# this seems to be flipped left-to-right.  Also, the legacy viewer is in some
+# kind of very sensitive (logarithmic or more?) brightness scale.
+matshow(get_trimmed_image(image, trim_quantile=1, num_sd=-Inf)); colorbar()
+WCS.pix_to_world(wcs, Float64[1398, 683])
 
